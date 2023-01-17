e47941019bf33abe98e01c701cc36d50e0cea476
GedeonGB <gedeonbaraka1@gmail.com>
2023-01-17 10:32:25 +0100

    Initial Commit

---
+++ TwendeTuh/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TwendeTuh
+//
+//  Created by Mac on 1/17/23.
+//  Copyright © 2023 Mac. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
